9a889ba140c66e869b60cc184501510649626569
beomsKim <beomsKim00@gmail.com>
2026-02-11 01:41:59 +0900

    chore: 게시판 폴더 디랙토리 수정
    - 글쓰기 (폴더 변경)
    - 수정/삭제 기능 (폴더 변경)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com', // Google 프로필 이미지
+      },
+    ],
+  },
+  // 성능 최적화
+  compress: true,
+  poweredByHeader: false,
+  // 프로덕션 빌드 최적화
+  swcMinify: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
